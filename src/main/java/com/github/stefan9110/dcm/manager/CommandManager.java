979370242d8ec9c194ecf8522d0877c20c665965
Centauri07 <magnus.centauri07@gmail.com>
2022-08-13 11:22:48 +0800

    clean imports

--- src/main/java/com/github/stefan9110/dcm/manager/CommandManager.java
+++ src/main/java/com/github/stefan9110/dcm/manager/CommandManager.java
@@ -33,7 +33,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class CommandManager {
     private Guild registeredGuild;
